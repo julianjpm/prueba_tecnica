7ab2e44b159f14f0531780422ab926a9fe1277e7
Julian Posada <julianposadam@gmail.com>
2023-07-03 23:00:02 -0500

    Se actualizan los ficheros

---
+++ src/main/java/com/ecommerce/service/UsuarioServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ecommerce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ecommerce.model.Usuario;
+import com.ecommerce.repository.UsuarioRepository;
+
+public class UsuarioServiceImpl implements UsuarioService {
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public Optional<Usuario> findById(Integer id) {
+		return usuarioRepository.findById(id);
+	}
+
+	@Override
+	public Usuario save(Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
+
+	@Override
+	public Optional<Usuario> findByEmail(String email) {
+		return usuarioRepository.findByEmail(email);
+	}
+
+	@Override
+	public List<Usuario> findAll() {
+		return usuarioRepository.findAll();
+	}
+}
